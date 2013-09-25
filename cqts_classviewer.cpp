6e88df8c7e6d244509c9646c52d6933d017f0b5a
Daniele Rapetti <iximiel@gmail.com>
2013-09-25 11:31:40 +0200

    Interface for classviewer done

--- cqts_classviewer.cpp
+++ cqts_classviewer.cpp
@@ -1,11 +1,31 @@
 #include "cqts_classviewer.h"
 #include <QLabel>
 #include <QComboBox>
-
+#include <QGridLayout>
 CQTs_ClassViewer::CQTs_ClassViewer(QWidget *parent) :
     QGroupBox(tr("Class Viewer"),parent)
 {
-    QLabel Sel(tr("Class:"));
-    QComboBox ComboSel();
+    QGridLayout *grid = new QGridLayout();
 
+    QLabel *Tlab = new QLabel(tr("Class:"));
+    grid->addWidget(Tlab,0,0);
+
+    Tlab = new QLabel(tr("Base Attack bonus:"));
+    grid->addWidget(Tlab,1,0);
+
+    QGroupBox *SavesBox= new QGroupBox(tr("Saves:"));
+    grid->addWidget(SavesBox,2,0,1,2);
+    QGridLayout *grid2 = new QGridLayout();
+    Tlab = new QLabel(tr("Fortitude:"));
+    grid2->addWidget(Tlab,0,0);
+    Tlab = new QLabel(tr("Reflexes"));
+    grid2->addWidget(Tlab,1,0);
+    Tlab = new QLabel(tr("Will:"));
+    grid2->addWidget(Tlab,2,0);
+    SavesBox->setLayout(grid2);
+
+    QComboBox *ComboSel = new QComboBox();
+    grid->addWidget(ComboSel,0,1);
+
+    setLayout(grid);
 }
