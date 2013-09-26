50f14cc5935dba9f77d7028ee3d38b94e122e02a
Daniele <iximiel@gmail.com>
2013-09-26 16:49:02 +0200

    need to test constructor for classhandler

--- cqts_engine.cpp
+++ cqts_engine.cpp
@@ -20,8 +20,9 @@ CQTs_engine::CQTs_engine()
  */
 
 CQTs_Class::CQTs_Class(QString classLink){
-    QFile file(classLink+".ClC");
+    QFile file(classLink+".ClC");//temporary
     int data;//data-sink for bab and ST
+    int F=16, R=8, W=4, bab=3;//mask for ST&BAB
     if (!file.open(QIODevice::ReadOnly | QIODevice::Text)){
         Name="error";
         data=0;
@@ -35,6 +36,11 @@ CQTs_Class::CQTs_Class(QString classLink){
         in >> lmax;
         in >> data;
     }
+    fort = (data & F)/F;
+    ref = (data & R)/R;
+    will = (data & W)/W;
+    fort = (data & F)/F;
+    bab = (data & bab)/bab;
 
 }
 
