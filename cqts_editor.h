d07dd59da688abc69c6a6d418bc39da2b36498db
Daniele Rapetti <iximiel@gmail.com>
2014-04-27 16:41:50 +0200

    forget a small thing

--- cqts_editor.h
+++ cqts_editor.h
@@ -4,6 +4,8 @@
 #include <QWidget>
 #include <QSpinBox>
 #include <QPushButton>
+#include <QLineEdit>
+#include "cqts_engine.h"
 
 class cqts_BABeditor : public QWidget
 {
@@ -33,6 +35,22 @@ private slots:
 private:
     QPushButton *saveBTT, *undoBTT;
     QSpinBox **newSpinST;
+};
+
+class cqts_Bioeditor : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit cqts_Bioeditor(int oldSTs[3], QWidget *parent = 0);
+    ~cqts_Bioeditor();
+signals:
+    void newSTs(int* ST);
+private slots:
+    void update();
+private:
+    QPushButton *saveBTT, *undoBTT;
+    QSpinBox *newSpinAge;
+    QLineEdit *newSpinName, *newSpinSurname;
 };
 
 #endif // CQTS_EDITOR_H
