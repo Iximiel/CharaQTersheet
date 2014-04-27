9203ee50f60697f1af035269cb54da6b3dc9475c
Daniele Rapetti <iximiel@gmail.com>
2014-04-27 16:39:50 +0200

    adding bioeditor

--- cqts_editor.cpp
+++ cqts_editor.cpp
@@ -10,7 +10,7 @@ cqts_BABeditor::cqts_BABeditor(int oldBAB, QWidget *parent):
 
     newSpinBaB = new QSpinBox();
     newSpinBaB->setValue(oldBAB);
-    grid->addRow("BaB:",newSpinBaB);
+    grid->addRow(tr("BaB:"),newSpinBaB);
 
     saveBTT = new QPushButton("&Save");
     undoBTT = new QPushButton("&Undo");
@@ -60,6 +60,43 @@ cqts_STeditor::~cqts_STeditor(){
 }
 
 void cqts_STeditor::update(){
+    int ST[3];
+    for (int i = 0; i < 3; ++i)
+        ST[i] = newSpinST[i]->value();
+
+    emit newSTs(ST);
+}
+
+//Bio
+cqts_Bioeditor::cqts_Bioeditor(int oldSTs[3],QWidget *parent):
+    QWidget(parent)
+{
+    QFormLayout *grid = new QFormLayout(this);
+
+    newSpinName = new QLineEdit();
+    grid->addRow(tr("Name:"),newSpinName);
+    newSpinSurname = new QLineEdit();
+    grid->addRow(tr("Surname:"),newSpinSurname);
+    newSpinAge = new QSpinBox();
+    grid->addRow(tr("Age:"),newSpinAge);
+
+
+    saveBTT = new QPushButton("&Save");
+    undoBTT = new QPushButton("&Undo");
+    QHBoxLayout *tLay= new QHBoxLayout();
+    tLay->addWidget(undoBTT);
+    tLay->addWidget(saveBTT);
+    connect(undoBTT,SIGNAL(clicked()),this,SLOT(close()));
+    connect(saveBTT,SIGNAL(clicked()),this,SLOT(update()));
+    grid->addRow(tLay);
+    setLayout(grid);
+}
+
+cqts_Bioeditor::~cqts_Bioeditor(){
+
+}
+
+void cqts_Bioeditor::update(){
     int ST[3];
     for (int i = 0; i < 3; ++i)
         ST[i] = newSpinST[i]->value();
