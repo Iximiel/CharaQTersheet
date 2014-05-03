b255f0c76795c5e7ccf8b1810cb65e4d0b2d7d99
Daniele Rapetti <iximiel@gmail.com>
2014-05-03 03:10:12 +0200

    FIrst idea on how items should work

--- items.cpp
+++ items.cpp
@@ -1,5 +1,12 @@
 #include "items.h"
 
+bool money::operator <(money x){
+    int I, X;
+    X = x.cu+(x.ag+(x.au+x.pt*10)*10)*10;
+    I = cu+(ag+(au+pt*10)*10)*10;
+    return I<X;
+}
+
 cqts_itemHandler::cqts_itemHandler(QObject *parent) :
     QObject(parent)
 {
--- items.h
+++ items.h
@@ -2,6 +2,32 @@
 #define ITEMS_H
 
 #include <QObject>
+struct money{
+    money(int mcu, int mag, int mau, int mpt);
+    int cu, ag, au, pt;
+    QDataStream operator <<();
+    money operator +(money);
+    money operator -(money);
+    bool operator <(money);
+};
+
+class cqts_item{
+public:
+    cqts_item(QString mycode,QString myname,int myweight, money myprice);
+    cqts_item(QString mycode,QString myname,int myweight, int mcu, int mag, int mau, int mpt);
+    money cost();
+    int myWeigh();
+    QString myID();
+    QString myName();
+    bool operator ==(cqts_item otherItem);
+    bool operator ==(QString otherCode);
+    bool operator <(cqts_item otherItem);//alphabetical order per name
+private:
+    QString code, name;
+    int weight;//, volume;
+    money price;
+};
+
 
 class cqts_itemHandler : public QObject
 {
