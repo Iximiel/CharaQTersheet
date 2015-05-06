47dcda35c57bfc77e7fe8683f31965d752ec9802
Daniele <iximiel@gmail.com>
2015-05-06 20:28:39 +0200

    Added buttons for skill weapon and armors

--- classcreator.cpp
+++ classcreator.cpp
@@ -2,6 +2,7 @@
 #include <QLabel>
 //#include <QGridLayout>
 #include <QFormLayout>
+#include <QPushButton>
 #include <QComboBox>
 #include <QLineEdit>
 
@@ -25,6 +26,15 @@ CQTs_ClassEditor::CQTs_ClassEditor(QWidget *parent)
     QComboBox *Combo_Ranks = new QComboBox();
     Combo_Ranks->addItems(List);
     grid->addRow(tLabel,Combo_Ranks);
+    QPushButton *BT_Weapon, *BT_Armor, *BT_Skills;
+    BT_Weapon = new QPushButton(tr("Set Weapons Proficiency"));
+    BT_Armor = new QPushButton(tr("Set Armor Proficiency"));
+    BT_Skills = new QPushButton(tr("Set Class Skills"));
+    BT_Weapon->setEnabled(false);
+    grid->addRow(BT_Weapon);
+    BT_Armor->setEnabled(false);
+    grid->addRow(BT_Armor);
+    grid->addRow(BT_Skills);
     setLayout(grid);
 }
 
